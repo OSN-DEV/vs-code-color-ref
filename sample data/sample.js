0fe1f753a39907215004223442a5ca3672544c3b
OSN-DEV <osndev1973@gmail.com>
2022-06-14 08:36:04 +0900

    add editor.wordHighlightBackground

---
+++ sample data/sample.js
@@ -0,0 +1,15 @@
+export {}
+
+let number: number[] = [1,2,3]
+
+let  numbers2: Array<number> = [1,2,3]
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto']
+
+let strings: string[] = ['TypeScript', 'Javascript', 'CoffeScript']
+
+let nh: number[][] = [
+  [50, 100],
+  [150, 200],
+]
+
+let vh:(string | number | boolean)[] = [1, false, 'Japan']
